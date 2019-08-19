0523a73315e52d97d1731fd64e71bdc2b6da3a33
yuchin <yuchinpao@liontravel.com>
2019-08-19 15:18:00 +0800

    Updates

---
+++ precache-manifest.65e4138bf1a8a75636a46a07d80c0594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c817200cc82230e257a7c30f3268c061",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "852df08761e380c3965b",
+    "url": "/repository-name/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "197c3b691178173062a6",
+    "url": "/repository-name/static/css/main.d8abf9d9.chunk.css"
+  },
+  {
+    "revision": "852df08761e380c3965b",
+    "url": "/repository-name/static/js/2.6e6c5f69.chunk.js"
+  },
+  {
+    "revision": "197c3b691178173062a6",
+    "url": "/repository-name/static/js/main.baa3d364.chunk.js"
+  },
+  {
+    "revision": "bcae02381800fbfd0149",
+    "url": "/repository-name/static/js/runtime~main.c2a15fe1.js"
+  }
+]);
